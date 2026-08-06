a383275ae6de1b849ae8ed04cf0fcb89c4c62611
Victor Manuel Ramirez Mendoza <victorman.rmz14@gmail.com>
2026-08-06 08:16:00 -0600

    fix(tests): actualizar ExampleTest para landing page publica

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertRedirect(route('dashboard', absolute: false));
+        $response->assertStatus(200);
     }
 }
